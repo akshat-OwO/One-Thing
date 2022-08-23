8bd427b48a0af477cb1c65cf97df902619b8d180
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-08-23 22:55:17 +0530

    Building the React Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import { ArrowCircleRightIcon } from '@heroicons/react/solid';
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [thing, setThing] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+    };
+
+    const handleInput = (e) => {
+        setThing(e.target.value);
+    };
 
     return (
-        <div className="App">
-            <h1>Hi</h1>
-        </div>
+        <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200">
+            <div className="grid place-items-center gap-8 m-8">
+                <h1 className="text-3xl sm:text-6xl font-bold text-center">
+                    What is your "One Thing"?
+                </h1>
+                <form
+                    className="flex ring-4 rounded-md ring-slate-200 dark:ring-slate-800 focus-within:ring-teal-600 focus-within:ring-offset-4 bg-slate-200 ring-offset-slate-200 dark:ring-offset-slate-800"
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        type="text"
+                        className="bg-inherit rounded-md font-sans text-slate-800 py-2 px-6 focus:outline-none text-xl sm:text-2xl placeholder:text-slate-400 caret-teal-600 appearance-none w-full"
+                        placeholder="Enter One Thing"
+                        autoFocus
+                        maxLength="64"
+                        value={thing}
+                        onInput={handleInput}
+                    />
+                    <button className="bg-inherit rounded-md font-sans text-slate-800 py-2 pr-6 focus:outline-none focus:text-teal-600 hover:text-teal-600">
+                        <ArrowCircleRightIcon className="h-12 w-12" />
+                    </button>
+                </form>
+            </div>
+        </main>
     );
 }
 
